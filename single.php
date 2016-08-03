0a5fd17f855821cc707b25e32c6502f69a54584a
Ruben Sanchez <rubensanchezcolin@gmail.com>
2016-08-03 02:35:08 -0500

    tags en single

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
                 <div class="row">
 
                     <div class="col-xs-12 col-sm-9">
-                        <?php //breadcrumbs(); ?>
+
                         <?php while ( have_posts() ) : the_post(); ?>
 
                             <div class="row">
@@ -54,6 +54,9 @@
                                 </div>
                             </div>
 
+                        <?php endwhile;?>
+
+
                             <div class="row compartir_post">
                                     <h5>Compartelo con tus amigos y corre la voz ;)</h5>
 
@@ -75,6 +78,37 @@
 
                             <?php  echo do_shortcode('[starbox] ') ;?>  
 
+                            <div class="row">
+                                <h1 class="articulos_tags"><i class="fa fa-hand-spock-o" aria-hidden="true"></i> Articulos recomendados</h1>
+                            <?php  
+                                //Articulos relacionador por etiquetas (tags)
+                                 $orig_post = $post;  
+                                 global $post;  
+                                 $tags = wp_get_post_tags($post->ID);  
+                                 if ($tags) {  
+                                    $tag_ids = array();  
+                                    foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;  
+                                    $args=array(  
+                                        'tag__in' => $tag_ids,  
+                                        'post__not_in' => array($post->ID),  
+                                        'posts_per_page'=>6,  
+                                        'caller_get_posts'=>1  
+                                    );  
+                                 $my_query = new wp_query( $args );  
+                                 while( $my_query->have_posts() ) {  
+                                    $my_query->the_post(); ?>                                  
+                                            <div class="col-xs-12 col-md-4">
+                                                <div class="imagen_tag">
+                                                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+                                                </div>
+                                               <div class="titulo_categoria"><a href="<?php the_permalink() ?>"><?php the_title('')?></a></div>  
+                                            </div>
+                                 <?php }  
+                                 }$post = $orig_post;  
+                                 wp_reset_query(); 
+                            ?>  
+                            </div>
+
 
 
                             <div class="fb-comments" data-href="<?php echo the_permalink() ?>" data-colorscheme="light" data-numposts="5"></div> 
@@ -82,31 +116,29 @@
                             .fb-comments span, .fb-comments iframe span[style], .fb-like-box span, .fb-like-box iframe span[style] {width: 100% !important;}
                             </style>     
 
-                        <?php endwhile;?>
 
                     </div> <!-- Fin contenido principal -->
 
                     <div class="col-xs-12 col-sm-3 sidebar">
+
                         <div class="row">
                             <div class="col-xs-12 widget">
                                 <h4><i class="fa fa-thumb-tack" aria-hidden="true"></i></i> Articulos relacionados</h4>
                         
                                 <?php
+                                  //Articulos relacionados por categoria
                                   global $post;
                                   $category = get_the_category($post->ID);
                                   $category = $category[0]->cat_ID;
                                   $myposts = get_posts(array('numberposts' =>'3', 'category__in' => array($category), 'post__not_in' => array($post->ID),'post_status'=>'publish'));
                                   foreach($myposts as $post) :
                                   setup_postdata($post);
-                                
                                   ?>
                                   
-                                   <div class="figure"> 
-                                        <?php the_post_thumbnail(); ?>
-
-                                       <div class="titulo_categoria"><a href="<?php the_permalink() ?>"><?php the_title('')?></a></div>  
-                                      
-                                    </div>
+                                       <div class="imagen_sidebar"> 
+                                           <?php the_post_thumbnail(); ?>
+                                           <div class="titulo_categoria"><a href="<?php the_permalink() ?>"><?php the_title('')?></a></div>  
+                                       </div>
 
                                 <?php wp_reset_postdata();?>
                                 <?php endforeach; ?>
@@ -120,19 +152,18 @@
                                     <?php
                                         query_posts('meta_key=post_views_count&orderby=meta_value_num&order=DESC&posts_per_page=5');
                                         if (have_posts()) : while (have_posts()) : the_post(); ?>
+                                               <div class="imagen_sidebar"> 
+                                                    <?php the_post_thumbnail(); ?>
 
-                                           <div class="figure"> 
-                                                <?php the_post_thumbnail(); ?>
-
-                                               <div class="titulo_categoria"><a href="<?php the_permalink() ?>"><?php the_title('')?></a></div>  
-                                              
-                                            </div>
+                                                   <div class="titulo_categoria"><a href="<?php the_permalink() ?>"><?php the_title('')?></a></div>  
+                                                </div>
                                         <?php
                                         endwhile; endif;
                                         wp_reset_query();
                                     ?>                                                            
                             </div>
                         </div>
+
                         <div class="row">
                             <div class="col-xs-12 widget">
 
@@ -141,7 +172,6 @@
                             
                             </div>
                         </div>
-
 
                         <div class="row">
                             <div class="col-xs-12 widget">
